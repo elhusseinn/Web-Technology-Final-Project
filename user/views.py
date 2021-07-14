5c77e154176a0de63694d262a795ab77589d18b1
Kareem Morsy <kreem.morsy@hotmail.com>
2021-07-14 06:37:59 +0200

    Added Sign in

--- user/views.py
+++ user/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-
 def register(request):
     return render(request, 'user/register.html')
+
+def signin(request):
+    return render(request, 'user/signin.html')
