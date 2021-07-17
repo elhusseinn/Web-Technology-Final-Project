328a9202fde7d0c71ef0c4888535b466e2ff06b4
Kareem Morsy <kreem.morsy@hotmail.com>
2021-07-17 18:05:30 +0200

    Added about us page

--- Home/views.py
+++ Home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-
     return render(request, 'Home/home.html')
 
-
+def about_us(request):
+    return render(request, 'Home/about-us.html')
